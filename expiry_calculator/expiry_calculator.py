b57f8a7e9049cf14f11cd222a53f9850beb23ee7
Vinayak Tripathi <vinayaktripathi22@gmail.com>
2025-03-09 14:16:45 +0530

    Fixed the bug for the Expiry Day

--- expiry_calculator/expiry_calculator.py
+++ expiry_calculator/expiry_calculator.py
@@ -58,24 +58,31 @@ class ExpiryCalculator:
         while date in self.holidays or date.weekday() >= 5:  # Skip weekends and holidays
             date -= pd.DateOffset(days=1)
         return date
-
+    
+    # Worked on v2 or the Expiry calculator as there was a bug in the code. The code failed for the 
+    # case 24Apr2025, 30Apr2025. Since 1stMay is holiday and is also first weekly expiry we need to consider
+    # 30Apr2025 as 1W expiry for the May Series and 24Apr2025 as Monthly expiry for Apr Series. But the
+    # code returned it as monthly expiry and the 24 Apr as 4W Expiry for Apr series. So fixed this here
     def get_all_expiry_dates(self):
         """Generate weekly and monthly expiry dates for Calls, handling holidays."""
-        weekly_expiry_dates = []
+
         years = self.year
         if isinstance(self.year, int):
             years = [self.year]
         elif not isinstance(self.year, list):
             raise ValueError("Years must be an integer or a list of integers.")
-        print(years)
+        weekly_expiry_dates = {}
+        monthly_expiry_dates = {}
         for year in years:
             for month in range(1, 13):
-                # Get all the expiry days for the given weekday in the month
+            # Get all the expiry days for the given weekday in the month
                 all_expiry_days = self.get_all_weekdays_in_month(month, year)
                 
                 # Adjust each weekly expiry date for holidays
+
                 adjusted_weekly_expiries = [self.get_trading_day_before(day) for day in all_expiry_days]
-                weekly_expiry_dates.extend(adjusted_weekly_expiries)
+                monthly_expiry_dates[adjusted_weekly_expiries[-1]] = month
+                weekly_expiry_dates = weekly_expiry_dates | {da:month for da in adjusted_weekly_expiries[:-1]}
                 
                 # # Assign 'W' for weekly expiries and 'M' for the last expiry of the month
                 # temp = ['W'] * len(adjusted_weekly_expiries)
@@ -83,43 +90,48 @@ class ExpiryCalculator:
                 # expiry_type.extend(temp)
         
         # Create a DataFrame with all expiry dates and expiry types
-        expiry_df = pd.DataFrame({
-            'ExpiryDay':self.expiry_day_name,
-            'Date': weekly_expiry_dates,
-            'ExpiryType': np.nan
-        })
+        week_expiry_df = pd.DataFrame.from_dict(weekly_expiry_dates, orient='index', columns=['ExpiryMonth']).reset_index().rename({"index":"Date"}, axis=1)
+        month_expiry_df = pd.DataFrame.from_dict(monthly_expiry_dates, orient='index', columns=['ExpiryMonth']).reset_index().rename({"index":"Date"}, axis=1)
+
+        week_expiry_df['Year'] = week_expiry_df['Date'].dt.year
+        month_expiry_df['Year'] = month_expiry_df['Date'].dt.year
+
+        week_expiry_df['WeekOfMonth'] = week_expiry_df.groupby(['Year','ExpiryMonth']).cumcount()+1
+        
+        # Assign <WeekNum>W for weekly expiries and 'M' for monthly expiry (last one in the month)
+        week_expiry_df['ExpiryType'] = week_expiry_df['WeekOfMonth'].astype('int',errors = 'ignore').astype(str) + 'W'  
+        month_expiry_df['ExpiryType'] = 'M'
+
+        expiry_df = pd.concat([week_expiry_df, month_expiry_df])
+
+        expiry_df['ExpiryMonth'] = pd.to_datetime(expiry_df['ExpiryMonth'],format='%m').dt.month_name()
+
         expiry_df["Month"] = expiry_df['Date'].dt.month_name()
         expiry_df['DayName'] = expiry_df['Date'].dt.day_name()
+        expiry_df = expiry_df.sort_values(by='Date')
 
-        expiry_df['Year'] = expiry_df['Date'].dt.year
-        expiry_df['WeekOfMonth'] = expiry_df['Date'].apply(lambda x: (x.day - 1) // 7 + 1)
-        
-        last_expiry_per_month = expiry_df.groupby(['Month','Year','ExpiryDay'])['Date'].apply(np.max).values
-
-        # Assign <WeekNum>W for weekly expiries and 'M' for monthly expiry (last one in the month)
-        expiry_df['ExpiryType'] = expiry_df['WeekOfMonth'].astype(str) + 'W'
-        expiry_df.loc[expiry_df['Date'].isin(last_expiry_per_month), 'ExpiryType'] = 'M'
         expiry_df.drop(['WeekOfMonth','Year'],  axis=1, inplace=True)
         
         return expiry_df  
     
     def get_monthly_expiry_dates(self):
         """Generate monthly expiry dates for Calls, handling holidays."""
-        weekly_expiry_dates = []
         years = self.year
         if isinstance(self.year, int):
             years = [self.year]
         elif not isinstance(self.year, list):
             raise ValueError("Years must be an integer or a list of integers.")
-        print(years)
+        monthly_expiry_dates = {}
         for year in years:
             for month in range(1, 13):
-                # Get all the expiry days for the given weekday in the month
+            # Get all the expiry days for the given weekday in the month
                 all_expiry_days = self.get_all_weekdays_in_month(month, year)
                 
                 # Adjust each weekly expiry date for holidays
+
                 adjusted_weekly_expiries = [self.get_trading_day_before(day) for day in all_expiry_days]
-                weekly_expiry_dates.extend(adjusted_weekly_expiries)
+                monthly_expiry_dates[adjusted_weekly_expiries[-1]] = month
+                # weekly_expiry_dates = weekly_expiry_dates | {da:month for da in adjusted_weekly_expiries[:-1]}
                 
                 # # Assign 'W' for weekly expiries and 'M' for the last expiry of the month
                 # temp = ['W'] * len(adjusted_weekly_expiries)
@@ -127,22 +139,26 @@ class ExpiryCalculator:
                 # expiry_type.extend(temp)
         
         # Create a DataFrame with all expiry dates and expiry types
-        expiry_df = pd.DataFrame({
-            'ExpiryDay':self.expiry_day_name,
-            'Date': weekly_expiry_dates,
-            'ExpiryType': np.nan
-        })
-        expiry_df["Month"] = expiry_df['Date'].dt.month_name()
-        expiry_df['DayName'] = expiry_df['Date'].dt.day_name()
+        # week_expiry_df = pd.DataFrame.from_dict(weekly_expiry_dates, orient='index', columns=['ExpiryMonth']).reset_index().rename({"index":"Date"}, axis=1)
+        month_expiry_df = pd.DataFrame.from_dict(monthly_expiry_dates, orient='index', columns=['ExpiryMonth']).reset_index().rename({"index":"Date"}, axis=1)
 
-        expiry_df['WeekOfMonth'] = expiry_df['Date'].apply(lambda x: (x.day - 1) // 7 + 1)
+        # week_expiry_df['Year'] = week_expiry_df['Date'].dt.year
+        month_expiry_df['Year'] = month_expiry_df['Date'].dt.year
+
+        # week_expiry_df['WeekOfMonth'] = week_expiry_df.groupby('ExpiryMonth').cumcount()+1
         
-        last_expiry_per_month = expiry_df.groupby('Month')['WeekOfMonth'].transform("max")
-
         # Assign <WeekNum>W for weekly expiries and 'M' for monthly expiry (last one in the month)
-        expiry_df['ExpiryType'] = expiry_df['WeekOfMonth'].astype(str) + 'W'
-        expiry_df.loc[expiry_df['WeekOfMonth'] == last_expiry_per_month, 'ExpiryType'] = 'M'
-        expiry_df.drop(['WeekOfMonth'],  axis=1, inplace=True)
-        
+        # week_expiry_df['ExpiryType'] = week_expiry_df['WeekOfMonth'].astype('int',errors = 'ignore').astype(str) + 'W'  
+        month_expiry_df['ExpiryType'] = 'M'
 
-        return expiry_df
+        # expiry_df = pd.concat([week_expiry_df, month_expiry_df])
+
+        month_expiry_df['ExpiryMonth'] = pd.to_datetime(month_expiry_df['ExpiryMonth'],format='%m').dt.month_name()
+
+        month_expiry_df["Month"] = month_expiry_df['Date'].dt.month_name()
+        month_expiry_df['DayName'] = month_expiry_df['Date'].dt.day_name()
+        month_expiry_df = month_expiry_df.sort_values(by='Date')
+
+        month_expiry_df.drop(['Year'],  axis=1, inplace=True)
+        
+        return month_expiry_df
